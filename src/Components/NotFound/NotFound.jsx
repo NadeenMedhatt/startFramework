a9f78c03e0345d269ab1b24edbf6260a103dc8f1
NadeenMedhatt <nadeenmedhat01@gmail.com>
2025-07-24 20:32:45 +0200

    handeling error

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function NotFound() {
-    return <>
-        <section className='vh-100'>
-            <h3>Page Not Found</h3>
-        </section>
-    
+  return (
+    <>
+      <section className="vh-100">
+        <h3>Page Not Found</h3>
+      </section>
     </>
+  );
 }
